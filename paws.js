133a86d58c29185ce0830a99aba23edac450de47
Susan Thompson <suejim74@gmail.com>
2022-03-09 13:30:14 -0500

    updated files. Organized css file.

--- paws.js
+++ paws.js
@@ -1,3 +1,5 @@
+
+/* errors
 <button id="hamburgerBtn">&#9776;</button><ul id="primaryNav">
     function toggleMenu(params) {
         document.getElementById('primaryNav').classList.toggle("open")
