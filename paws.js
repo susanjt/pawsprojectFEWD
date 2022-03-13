ea7de16cc32f5e0fbd709d7422ad7b874b528182
Susan Thompson <suejim74@gmail.com>
2022-03-12 22:22:25 -0500

    updated files, new images added

--- paws.js
+++ paws.js
@@ -1,8 +1,19 @@
+//hamburger tog
 
-/* errors
-<button id="hamburgerBtn">&#9776;</button><ul id="primaryNav">
-    function toggleMenu(params) {
-        document.getElementById('primaryNav').classList.toggle("open")
-    }
-let x= document.getElementById('hamburgerBtn');
-x.onclick= toggleMenu;
+const toggleButton = document.getElementById ('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+toggleButton.addEventListener('click', () => {
+    primaryNav.classList.toggle('active')
+});
+
+
+//submit  name email
+
+const submitBtn = document.getElementById('btn');
+
+    submitBtn = addEventListener('click', (e) => {
+    e.preventDefault();
+
+    submitBtn.innerText = 'Submitted!';
+});
