767ab0f0a22bdc8c745b6c78764d4a18f09b958f
Susan Thompson <suejim74@gmail.com>
2022-03-27 20:38:07 -0400

    Updated files. form validation in js. added links. changed styles.

--- paws.js
+++ paws.js
@@ -17,11 +17,66 @@ function closeMenu() {
 
 //submit  name email
 
-const submitBtn = document.getElementById('btn');
+const submitBtn = document.querySelector('#submit');
+const firstName = document.querySelector('#first-name');
+const lastName = document.querySelector('#last-name');
+const email = document.querySelector('#email');
 
-    submitBtn.addEventListener('click', (e) => {
-    e.preventDefault();
+const fName = document.querySelector('input');
 
-    submitBtn.innerText = 'Submitted!';
+fName.addEventListener('input', () =>{
+  fName.setCustomValidity('');
+  fName.checkValidity();
+  console.log(fName.checkValidity());
+  if(fName.validity.valid == true){
+    fName.style.borderColor = '#13315c';
+  }
 });
 
+fName.addEventListener('invalid', () => {
+    fName.setCustomValidity('Not a valid entry');
+    fName.style.borderColor = '#832232';
+  });
+  
+  const lName = document.querySelector('#last-name');
+  
+  lName.addEventListener('input', () =>{
+    lName.setCustomValidity('');
+    lName.checkValidity();
+    console.log(lName.checkValidity());
+    if(lName.validity.valid == true){
+      lName.style.borderColor = '#13315c';
+    }
+  });
+
+  lName.addEventListener('invalid', () => {
+    lName.setCustomValidity('NICE TRY');
+    lName.style.borderColor = '#832232';
+  });
+
+  //const email = document.querySelector('#email');
+
+email.addEventListener('input', () =>{
+  email.setCustomValidity('');
+  email.checkValidity();
+  console.log(email.checkValidity());
+  if(email.validity.valid == true){
+    email.style.borderColor = '#13315c';
+
+    email.addEventListener('invalid', () => {
+        email.setCustomValidity(`NOT TODAY`);
+        email.style.borderColor = '#832232';
+      });
+      
+
+submitBtn.addEventListener('click', () => {
+        submitBtn.textContent = 'Submitted';
+        console.log(`
+        First Name: ${fName.value}
+        Last Name: ${lName.value}
+        Email: ${email.value}`)
+});
+  }
+});
+
+//submitBtn.innerText = 'Submitted!';
